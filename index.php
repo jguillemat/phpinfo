a3a68e151e44e7643fc47b5f9be618184a0b553d
jguillemat <jguillemat@users.noreply.github.com>
2018-06-21 12:42:51 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,10 @@
 // Show all information, defaults to INFO_ALL
 echo "Se acabo el phpinfo";
 
+
+echo gethostname(); //puede imprimir: sandie
+
+// O, una opción que también funciona antes de PHP 5.3
+echo php_uname('n'); //puede imprimir: sandie
+
 ?>
